ab9adb68c1e0418d8545d95e6675c292152ce9ac
Saddam <saddam12@gmail.com>
2021-12-01 15:32:23 +0530

    dashboard home page mobile responsive  Updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 //import { useHistory  } from "react-router-dom";
 import { getUser, removeUserSession } from "../Utils/Common";
-import { Navbar,Container,Col,Row,Nav,NavDropdown, Button} from "react-bootstrap";
+import { useState } from "react";
+import { Navbar,Container,Col,Row,Nav,NavDropdown, Button,Offcanvas,} from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Redirect,Route } from "react-router-dom";
 import {Link} from "react-router-dom";
 import "./Home.css";
@@ -15,7 +16,54 @@ import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
 import SportsVolleyballIcon from '@material-ui/icons/SportsVolleyball';
 import FormatIndentDecreaseIcon from '@material-ui/icons/FormatIndentDecrease';
 import VideogameAssetIcon from '@material-ui/icons/VideogameAsset';
+import { useTheme } from '@material-ui/core'
+import { useMediaQuery } from '@material-ui/core';
+import DehazeIcon from '@material-ui/icons/Dehaze';
+
 export const Home = (props) => {
+  function LeftNav(){
+    return(
+<>
+<ul>
+  <li><Link class="active" to="/dashboard"><TableChartIcon></TableChartIcon> dashboard</Link></li>
+  <li><Link to="/sport"><SportsEsportsIcon></SportsEsportsIcon> Sport</Link></li>
+  <li><Link to="/tournament"><SportsVolleyballIcon></SportsVolleyballIcon>Tournament</Link></li>
+  <li><Link to="/category"><FormatIndentDecreaseIcon></FormatIndentDecreaseIcon>Category</Link></li>
+</ul> 
+
+</>
+
+
+    )
+  }
+  function Show(){
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return(
+      <>
+       
+      <DehazeIcon style={{fontSize:"50px"}} onClick={handleShow}></DehazeIcon>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+        <ul>
+       <li onClick={handleClose}><Link class="active" to="/dashboard"><TableChartIcon></TableChartIcon> dashboard</Link></li>
+       <li  onClick={handleClose}><Link to="/sport"><SportsEsportsIcon></SportsEsportsIcon> Sport</Link></li>
+        <li  onClick={handleClose}><Link to="/tournament"><SportsVolleyballIcon></SportsVolleyballIcon>Tournament</Link></li>
+       <li  onClick={handleClose} ><Link to="/category"><FormatIndentDecreaseIcon></FormatIndentDecreaseIcon>Category</Link></li>
+      </ul> 
+        </Offcanvas.Body>
+      </Offcanvas>
+      </>
+    )
+  }
+ 
+  const theme = useTheme();
+  const chk = useMediaQuery(theme.breakpoints.down("md"))
 
      
   const user = JSON.parse(localStorage.getItem('user'));
@@ -32,7 +80,7 @@ export const Home = (props) => {
     <div className="b-border">
     <Navbar className="Nav-res">
  
-    <Navbar.Brand href="#home"><VideogameAssetIcon/> Admin Dashboard</Navbar.Brand>
+    <Navbar.Brand > {chk==true?<Show/>:"Admin Dashboard"} </Navbar.Brand>
     <Navbar.Toggle />
     <Navbar.Collapse className="justify-content-end">
       <Navbar.Text>
@@ -46,15 +94,10 @@ export const Home = (props) => {
 <div >
   <div className="container-fluid">
   <Row>
-    <Col sm={12} md={2}  >
-      <ul>
-  <li><Link class="active" to="/dashboard"><TableChartIcon></TableChartIcon> dashboard</Link></li>
-  <li><Link to="/sport"><SportsEsportsIcon></SportsEsportsIcon> Sport</Link></li>
-  <li><Link to="/tournament"><SportsVolleyballIcon></SportsVolleyballIcon>Tournament</Link></li>
-  <li><Link to="/category"><FormatIndentDecreaseIcon></FormatIndentDecreaseIcon>Category</Link></li>
-</ul> 
+    <Col xs={12} md={2} xl={2}  >
+    {chk==true?"":<LeftNav/>}
     </Col>
-    <Col sm={12} md={10} id="right-box" >
+    <Col xs={12} xl={10} md={10} id="right-box" >
     <div >
           <Switch>
            <Route exact path="/dashboard" component={Dashboard} />
